24d3ed608326577cfac481adec1e8ef19f6aebf2
755-LinuxPadawan <53091135+755-linuxpadawan@users.noreply.github.com>
2020-04-14 15:22:27 +0100

    rectiflst

--- rootless_podman.sh
+++ rootless_podman.sh
@@ -7,26 +7,29 @@
 #Voir aussi ce tuto auquel je me suis servi pour creer le script: https://critchlow.xyz/2019/09/23/rootless-podman.html
 
 #Ask nom user a add pour subgid et subuid
-read -p "Quel est l'user a add dans les fichiers subgid/subuid ? " myUserName
-
-echo ${myUserName}
+read -p "Quel est l'user a add dans les fichiers subgid/subuid ?" myUserName
 
 if [ ! -f /etc/sysctl.d/userns.conf ]
 then
-    echo "kernel.unprivileged_userns_clone=1" > /etc/sysctl.d/userns.conf
+echo "kernel.unprivileged_userns_clone=1" > /etc/sysctl.d/userns.conf
 else
-    echo "Verifier si kernel.unprivileged_userns_clone=1 dans le fichier [ /etc/sysctl.d/userns.conf ]"
+echo "Verifier si kernel.unprivileged_userns_clone=1 dans le fichier [ /etc/sysctl.d/userns.conf ]"
 fi
-    
+
 if [ ! -f /etc/subgid ] && [ ! -f /etc/subuid ]
 then
     echo "Creation des fichiers subgid et subuid en cours..."
     touch /etc/subgid
     touch /etc/subuid
+    echo "Ajout de l'utisateur avec extension de uid/gid en cours..."
+    usermod --add-subuids 100000-165535 ${myUserName}
+    usermod --add-subgids 100000-165535 ${myUserName}
+    echo ""
+    cat /etc/sub*
+ else
     echo "Ajout de l'uitisateur avec extension de uid/gid en cours..."
     usermod --add-subuids 100000-165535 ${myUserName}
     usermod --add-subgids 100000-165535 ${myUserName}
-else
-    usermod --add-subuids 100000-165535 ${myUserName}
-    usermod --add-subgids 100000-165535 ${myUserName}
+    echo ""
+    cat /etc/sub*
 fi
